df58739e7d495feaea9be970d3b2e4d9a576a388
Obi <mueedrazamr1@gmail.com>
2025-06-05 23:50:49 +0530

    CRUD Oprations completed

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -6,8 +7,12 @@ namespace BulkyWeb.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage =" Category Name is Required")]
+        [DisplayName("Category Name")]
+        [MaxLength(25)]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
 
     }
